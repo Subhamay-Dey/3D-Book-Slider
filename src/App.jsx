2ccffae28e83790cd864ecec2d3632d6afed07a4
Subhamay-Dey <ncdey1966@gmail.com>
2025-06-20 20:09:41 +0530

    inital commit - React project setup done and all code cleaned up ✅

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { Experience } from "./components/Experience";
+import { UI } from "./components/UI";
+
+function App() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default App;
